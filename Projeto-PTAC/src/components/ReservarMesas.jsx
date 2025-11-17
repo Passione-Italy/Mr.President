15e30fa0c7de30851875eb22a8076bfb7b96cc63
kanivetuchiha <erickazambuja.ms@gmail.com>
2025-11-17 14:18:08 -0400

    update

--- Projeto-PTAC/src/components/ReservarMesas.jsx
+++ Projeto-PTAC/src/components/ReservarMesas.jsx
@@ -1,5 +1,25 @@
+import '../CSS/reservarMesa.css'
+
 export default function ReservarMesas () {
     return(
-        <h1>reserva de mesa</h1>
+        <div className="container">
+
+<form>
+
+    <label>Insira a data que desaja reservar</label>
+<input type="date"/>
+    <label>Insira o horario da reserva</label>
+<input type="time"/>
+    <label>Insira seu nome</label>
+<input type="text"/>
+    <label>Insira um numero de contato</label>
+<input type="number"/>
+    <label>Insira o numero da mesa</label>
+<input type="number"/>
+
+    <button type="submit">ENVIAR</button>
+</form>
+
+        </div>
     )
 }
--- Projeto-PTAC/src/components/VerMinhasReservas.jsx
+++ Projeto-PTAC/src/components/VerMinhasReservas.jsx
@@ -1,32 +1,21 @@
-import { useState } from "react";
 import '../CSS/verReserva.css'
 export default function ConsultarReservas() {
-    const [numeroMesa, setNumeroMesa] = useState(0);
-    const [statusMesa, setStatusMesa] = useState(false);
-    const [capacidade, setCapacidade] = useState(0);
-
 
  return (
-    <div>
-        <h1 id="Title_Consultar">Consultar Reservas</h1>
-        <form>
-            
-            <div class="input_consultar">
-                <label htmlFor="numeroMesa">N° da mesa:</label>
-                <input type="number" id="numeroMesa" name="numeroMesa" value onChange={(value) => setCapacidade(value)} required />
-            </div>
-            <div class="input_consultar">
-                 <label htmlFor="statusMesa">Status da mesa:</label>
-                  <input type="string" id="nomeCliente" name="nomeCliente" value onChange={(value) => setCapacidade(value)} required />
-            </div>
-            <div class="input_consultar">
-                <label htmlFor="capacidade">Capacidade da Mesa:</label>
-                 <input type="number" id="capacidade" name="capacidade" value onChange={(value) => setCapacidade(value)} required />
-            </div>
-            <br />
-            <button type="submit">Consultar</button>
-            <button type="clear">Limpar</button>
-        </form>
+    <div className="container">
+<form>
+
+    <label>Insira o Nº da mesa</label>
+<input type="number"/>
+
+
+    <button>Consultar</button>
+    <button type="submit">Limpar</button>
+</form>
+<div className='infos'>
+    <p>Status: Reservado</p> <br/>
+    <p>capacidade: 4 pessoas</p>
+</div>
     </div>
   )
 }
